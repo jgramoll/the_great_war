b2ba880a09fcdf0e4a96690febe2f0ee7dec371b
jgramoll <jgramoll@instructure.com>
2017-06-13 22:54:57 -0600

    remove hello world references

--- client/app/bundles/GameList/startup/registration.jsx
+++ client/app/bundles/GameList/startup/registration.jsx
@@ -4,15 +4,9 @@ import GameListApp from './GameListApp';
 import { addLocaleData } from 'react-intl';
 import en from 'react-intl/locale-data/en';
 import ru from 'react-intl/locale-data/ru';
-// import { translations } from 'path_to/i18n/translations';
-// import { defaultLocale } from 'path_to/i18n/default';
 
 // Initizalize all locales for react-intl.
 addLocaleData([...en, ...ru]);
-
-// set locale and messages for IntlProvider.
-// const locale = method_to_get_current_locale() || defaultLocale;
-// const messages = translations[locale];
 
 ReactOnRails.register({
   GameListApp,
