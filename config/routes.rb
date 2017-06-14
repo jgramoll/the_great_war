b3b867e80a93078e65ebfbc6310e0e63053bef8e
jgramoll <jgramoll@instructure.com>
2017-06-13 21:24:59 -0600

    default route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :games, only: [:create, :update, :show, :destroy],
     constraints: lambda { |req| req.format == :json }
 
-  root 'games#index'
+  # root 'games#index'
 end
