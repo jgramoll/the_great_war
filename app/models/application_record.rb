14c26a593b9353494b82d23c9b0bc435cd9a3f00
jgramoll <jgramoll@instructure.com>
2017-06-29 09:34:12 -0600

    add documentation and inch badge

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,16 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  # force ids to be serialized as string
+  # Public: force ids to be serialized as string
+  #
+  # args - Hash arguments for configuration
+  #
+  # Examples
+  #
+  #   Game.as_json
+  #   # => { id: '1' }
+  #
+  # Returns Hash
   def as_json(*args)
     super.merge('id' => id.to_s)
   end
