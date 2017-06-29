5572c73a3c7738fd4a2dac7b33033464941cf083
jgramoll <jgramoll@instructure.com>
2017-06-29 10:36:42 -0600

    update game controller docs

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,65 +3,122 @@ class GamesController < ApplicationController
   before_action :set_game, only: %i[show edit update destroy]
   before_action :authenticate_user!
 
+  # Public: Get list of all games
   # GET /games
   # GET /games.json
+  # 
+  # Examples
+  #   # => [
+  #     {
+  #       id: '1',
+  #       name: 'Demo',
+  #       gameType: 'Demo'
+  #     },
+  #     ...
+  #   ]
+  #
+  # Returns the Array of Game Records
   def index
     @games = Game.all.order(created_at: :desc)
     @games_loaded = true
   end
 
+  # Public: Get html form for new game
   # GET /comments/new
+  #
+  # Returns Html
   def new
     render :index
   end
 
+  # Public: Get a game by id
+  # GET /games/1
   # GET /games/1.json
+  #
+  # id - id of the record
+  # 
+  # Examples
+  #   # => {
+  #     id: '1',
+  #     name: 'Demo',
+  #     gameType: 'Demo'
+  #   }
+  #
+  # Returns Game Record
   def show
     @selected_game = Game.find(params.require(:id))
     render :index
   end
 
+  # Public: Create a game
   # POST /games.json
+  #
+  # name - name of the record
+  # gameType - type of game
+  # 
+  # Examples
+  #   {
+  #     name: 'Demo',
+  #     gameType: 'Demo'
+  #   }
+  #   # => {
+  #     id: '1'
+  #     name: 'Demo',
+  #     gameType: 'Demo'
+  #   }
+  #
+  # Returns Game Record
   def create
     @game = Game.new(game_params.merge(host: current_user))
 
-    respond_to do |format|
-      if @game.save
-        format.json { render :show, status: :created, location: @game }
-      else
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
+    if @game.save
+      render :show, status: :created, location: @game
+    else
+      render json: @game.errors, status: :unprocessable_entity
     end
   end
 
+  # Public: Update a game
   # PATCH/PUT /games/1.json
+  #
+  # name - name of the record
+  # 
+  # Examples
+  #   {
+  #     name: 'New Name'
+  #   }
+  #   # => {
+  #     id: '1'
+  #     name: 'New Name',
+  #     gameType: 'Demo'
+  #   }
+  #
+  # Returns Game Record
   def update
-    respond_to do |format|
-      if @game.update(game_params)
-        format.json { render :show, status: :ok, location: @game }
-      else
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
+    if @game.update(game_params)
+      render :show, status: :ok, location: @game
+    else
+      render json: @game.errors, status: :unprocessable_entity
     end
   end
 
+  # Public: Delete a game
   # DELETE /games/1.json
+  #
+  # Returns No Content
   def destroy
     @game.destroy
-    respond_to do |format|
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
+  # Private: set game attr before actions
   def set_game
     @game = Game.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet,
-  # only allow the white list through.
+  # Private: Game params permitted
   def game_params
     params.fetch(:game, {}).permit(:name, :game_type)
   end
